0d3e78aaeb7aa2de0044853cd0c60e1890530335
Maple IT Firm <mapleitfirm.pc12@gmail.com>
2023-07-22 10:10:26 +0600

    warning fixed

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,48 +1,76 @@
-import React from 'react'
-import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
-
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contacts = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
-      <div className='container contact__container'>
+      <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>baktiar.kabir.joy3008@gmail.com</h5>
-            <a href="mailto:baktiar.kabir.joy3008@gmail.com" target='_blank'>Send a Message</a>
+            <a
+              href="mailto:baktiar.kabir.joy3008@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
           </article>
 
           <arcticle className="contact__option">
-            <RiMessengerLine className='contact__option-icon'/>
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Baktiar Kabir Joy</h5>
-            <a href="https://m.me/baktiarkabir.joy/" target='_blank'>Send a Message</a>
+            <a
+              href="https://m.me/baktiarkabir.joy/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
           </arcticle>
 
           <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon'/>
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>01533546542</h5>
-            <a href="https://api.whatsapp.com/send?phone= +8801533546542" target='_blank'>Send a Message</a>
+            <a
+              href="https://api.whatsapp.com/send?phone= +8801533546542"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
           </article>
         </div>
         <form action="">
-          <input type="text" name="name" placeholder='Your Full Name' required />
-          <input type="email" name="email" placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
-          <button type="submit" className='btn btn-primary'>Send Message</button>
-
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {BsFacebook} from 'react-icons/bs'
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsFacebook } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/baktiar-kabir-joy/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/Kabir3008" target="_blank"><FaGithub/></a>
-        <a href="https://www.facebook.com/baktiarkabir.joy/" target="_blank"> <BsFacebook/></a>
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/baktiar-kabir-joy/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/Kabir3008" target="_blank" rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.facebook.com/baktiarkabir.joy/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        <BsFacebook />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
